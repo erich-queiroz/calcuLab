d04d90c1fd34d7ec425c5dd297fe4b421bb7f02f
Erich Queiroz <95364468+erich-queiroz@users.noreply.github.com>
2023-05-22 11:08:42 -0300

    add initial archives
    include stylesheet, index of html, percent calculator js

---
+++ program/app-percent.js
@@ -0,0 +1,21 @@
+const form = document.getElementById('percentageForm');
+    const percentageInput = document.getElementById('percentageInput');
+    const amountInput = document.getElementById('amountInput');
+    const resultElement = document.getElementById('result');
+
+    form.addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      const percentage = parseFloat(percentageInput.value);
+      const amount = parseFloat(amountInput.value);
+
+      if (!isNaN(percentage) && !isNaN(amount)) {
+        resultElement.style.color = 'black';
+        const result = amount - ((percentage / 100) * amount);
+        resultElement.innerHTML = `<b>${percentage}% de R$ ${amount} reais é igual a: ${result.toFixed(2)} reais</b>. <p>Logo, o desconto aplicado será: <b>${amount - result}</b> reais.</p>`;
+      } else {
+        
+        resultElement.textContent = 'Valores inválidos. Por favor, insira apenas números.';
+        resultElement.style.color = 'red';
+      }
+    });
